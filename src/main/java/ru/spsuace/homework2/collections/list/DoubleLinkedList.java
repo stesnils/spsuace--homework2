f7ce0ab13b1469e56c2caa1f8cecc7241d80fb90
alexander.galkin <alexander.galkin@corp.mail.ru>
2019-02-20 10:32:00 +0300

    Added tests for list

--- src/main/java/ru/spsuace/homework2/collections/list/DoubleLinkedList.java
+++ src/main/java/ru/spsuace/homework2/collections/list/DoubleLinkedList.java
@@ -6,54 +6,55 @@ import java.util.Iterator;
  * Реализовать двусвязный список, аналог LinkedList в java (то что я рассказывал на лекции)
  * Если не понятно, что должны возвращать методы, смотрите документацию интерфейсов List и Collection
  */
-public class DoubleLinkedList<T> {
+public class DoubleLinkedList<T> implements Iterable<T> {
 
     public int size() {
         return 0;
     }
 
-    boolean contains(Object o) {
+    public boolean contains(Object o) {
         return false;
     }
 
-    void clear() {
+    public void clear() {
 
     }
 
-    void add(int index, T element) {
+    public void add(int index, T element) {
 
     }
 
-    void addLast(T element) {
-
-    }
-
-
-    void addFirst(T element) {
+    public void addLast(T element) {
 
     }
 
 
-    T set(int index, T element) {
+    public void addFirst(T element) {
+
+    }
+
+
+    public T set(int index, T element) {
         return null;
     }
 
-    T get(int index) {
+    public T get(int index) {
         return null;
     }
 
-    int indexOf(T o) {
+    public int indexOf(T o) {
         return -1;
     }
 
-    T remove(int index) {
+    public T remove(int index) {
         return null;
     }
 
     /**
      * Дополнительное задание
      */
-    Iterator<T> iterator() {
+    @Override
+    public Iterator<T> iterator() {
         return null;
     }
 }
