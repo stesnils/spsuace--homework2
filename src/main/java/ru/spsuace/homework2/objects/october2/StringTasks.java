9b54099289484c91b6105a61fb38b5b59bb6ed18
Aleksandr Galkin <alexander.galkin@corp.mail.ru>
2019-10-04 23:05:10 +0300

    added new hometask

---
+++ src/main/java/ru/spsuace/homework2/objects/october2/StringTasks.java
@@ -0,0 +1,23 @@
+package ru.spsuace.homework2.objects.october2;
+
+public class StringTasks {
+
+    /**
+     * Убрать все лишние символы из строки и вернуть получившееся число.
+     * Разрешенные символы: цифры, '-', '.', 'e'
+     * Если '.' и 'e' больше чем 1, возвращаем null
+     * Любой класс-обертка над примитивами наследуется от Number
+     * Можно использовать функции Double.valueOf() и другие такие же
+     */
+    public static Number simpleValueOf(String str) {
+        return null;
+    }
+
+
+    /**
+     * Тоже самое, что и выше, но нельзя пользоваться функциями valueOf()
+     */
+    public static Number valueOf(String str) {
+        return null;
+    }
+}
---
+++ src/test/java/ru/spsuace/homework2/objects/october2/StringTasksTest.java
@@ -0,0 +1,50 @@
+package ru.spsuace.homework2.objects.october2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringTasksTest {
+
+    @Test
+    public void simpleValueOf() {
+        System.out.println(Integer.MIN_VALUE);
+        assertEquals(null, StringTasks.simpleValueOf(null));
+        assertEquals(null, StringTasks.simpleValueOf(""));
+        assertEquals(0, StringTasks.simpleValueOf("0"));
+        assertEquals(0, StringTasks.simpleValueOf("a0"));
+        assertEquals(-1, StringTasks.simpleValueOf("-a1"));
+        assertEquals(null, StringTasks.simpleValueOf("--a1"));
+        assertEquals(-12, StringTasks.simpleValueOf("-a1bsc2"));
+        assertEquals(132, StringTasks.simpleValueOf("a-132a"));
+        assertEquals(2147483649L, StringTasks.simpleValueOf("asddgsdf2fg1474asdfg8364asd9asd"));
+        assertEquals(-2147483649L, StringTasks.simpleValueOf("asd-dgsdf2fg1474asdfg8364asd9asd"));
+        assertEquals(1e2, StringTasks.simpleValueOf("1e2"));
+        assertEquals(-1e-2, StringTasks.simpleValueOf("asd-asd1asddecvxv-dsf2fsdv"));
+        assertEquals(1e-3, StringTasks.simpleValueOf("1e-asdsad3"));
+        assertEquals(1.2e-3, StringTasks.simpleValueOf("ccz1asd.zc2deg-h3j"));
+        assertEquals(null, StringTasks.simpleValueOf("ccz1asd.zc2de.g-h3j"));
+        assertEquals(1.3, StringTasks.simpleValueOf("fff1fdf.asdsad3"));
+    }
+
+    @Test
+    public void valueOf() {
+        System.out.println(Integer.MIN_VALUE);
+        assertEquals(null, StringTasks.simpleValueOf(null));
+        assertEquals(null, StringTasks.simpleValueOf(""));
+        assertEquals(0, StringTasks.simpleValueOf("0"));
+        assertEquals(0, StringTasks.simpleValueOf("a0"));
+        assertEquals(-1, StringTasks.simpleValueOf("-a1"));
+        assertEquals(null, StringTasks.simpleValueOf("--a1"));
+        assertEquals(-12, StringTasks.simpleValueOf("-a1bsc2"));
+        assertEquals(132, StringTasks.simpleValueOf("a-132a"));
+        assertEquals(2147483649L, StringTasks.simpleValueOf("asddgsdf2fg1474asdfg8364asd9asd"));
+        assertEquals(-2147483649L, StringTasks.simpleValueOf("asd-dgsdf2fg1474asdfg8364asd9asd"));
+        assertEquals(1e2, StringTasks.simpleValueOf("1e2"));
+        assertEquals(-1e-2, StringTasks.simpleValueOf("asd-asd1asddecvxv-dsf2fsdv"));
+        assertEquals(1e-3, StringTasks.simpleValueOf("1e-asdsad3"));
+        assertEquals(1.2e-3, StringTasks.simpleValueOf("ccz1asd.zc2deg-h3j"));
+        assertEquals(null, StringTasks.simpleValueOf("ccz1asd.zc2de.g-h3j"));
+        assertEquals(1.3, StringTasks.simpleValueOf("fff1fdf.asdsad3"));
+    }
+}
