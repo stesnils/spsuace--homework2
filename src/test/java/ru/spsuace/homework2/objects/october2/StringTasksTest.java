cc028415a0e1db951d85542ac97b91b57317dda4
Aleksandr Galkin <alexander.galkin@corp.mail.ru>
2019-10-08 22:14:58 +0300

    fix test and added comment

--- src/test/java/ru/spsuace/homework2/objects/october2/StringTasksTest.java
+++ src/test/java/ru/spsuace/homework2/objects/october2/StringTasksTest.java
@@ -8,7 +8,6 @@ public class StringTasksTest {
 
     @Test
     public void simpleValueOf() {
-        System.out.println(Integer.MIN_VALUE);
         assertEquals(null, StringTasks.simpleValueOf(null));
         assertEquals(null, StringTasks.simpleValueOf(""));
         assertEquals(0, StringTasks.simpleValueOf("0"));
@@ -17,11 +16,17 @@ public class StringTasksTest {
         assertEquals(null, StringTasks.simpleValueOf("--a1"));
         assertEquals(-12, StringTasks.simpleValueOf("-a1bsc2"));
         assertEquals(-132, StringTasks.simpleValueOf("a-132a"));
+        assertEquals(-2147483648, StringTasks.simpleValueOf("a-d21s474asd83648sad"));
         assertEquals(2147483649L, StringTasks.simpleValueOf("asddgsdf2fg1474asdfg8364asd9asd"));
         assertEquals(-2147483649L, StringTasks.simpleValueOf("asd-dgsdf2fg1474asdfg8364asd9asd"));
         assertEquals(1e2, StringTasks.simpleValueOf("1e2"));
         assertEquals(-1e-2, StringTasks.simpleValueOf("asd-asd1asddecvxv-dsf2fsdv"));
         assertEquals(1e-3, StringTasks.simpleValueOf("1e-asdsad3"));
+        assertEquals(null, StringTasks.simpleValueOf("1-easdsad3"));
+        assertEquals(null, StringTasks.simpleValueOf("1e--asdsad3"));
+        assertEquals(null, StringTasks.simpleValueOf("--1easdsad3"));
+        assertEquals(null, StringTasks.simpleValueOf("-1e-asdsad3-"));
+        assertEquals(null, StringTasks.simpleValueOf("1easdsad3-"));
         assertEquals(1.2e-3, StringTasks.simpleValueOf("ccz1asd.zc2deg-h3j"));
         assertEquals(null, StringTasks.simpleValueOf("ccz1asd.zc2de.g-h3j"));
         assertEquals(1.3, StringTasks.simpleValueOf("fff1fdf.asdsad3"));
@@ -29,7 +34,6 @@ public class StringTasksTest {
 
     @Test
     public void valueOf() {
-        System.out.println(Integer.MIN_VALUE);
         assertEquals(null, StringTasks.simpleValueOf(null));
         assertEquals(null, StringTasks.simpleValueOf(""));
         assertEquals(0, StringTasks.simpleValueOf("0"));
@@ -38,11 +42,17 @@ public class StringTasksTest {
         assertEquals(null, StringTasks.simpleValueOf("--a1"));
         assertEquals(-12, StringTasks.simpleValueOf("-a1bsc2"));
         assertEquals(-132, StringTasks.simpleValueOf("a-132a"));
+        assertEquals(-2147483648, StringTasks.simpleValueOf("a-d21s474asd83648sad"));
         assertEquals(2147483649L, StringTasks.simpleValueOf("asddgsdf2fg1474asdfg8364asd9asd"));
         assertEquals(-2147483649L, StringTasks.simpleValueOf("asd-dgsdf2fg1474asdfg8364asd9asd"));
         assertEquals(1e2, StringTasks.simpleValueOf("1e2"));
         assertEquals(-1e-2, StringTasks.simpleValueOf("asd-asd1asddecvxv-dsf2fsdv"));
         assertEquals(1e-3, StringTasks.simpleValueOf("1e-asdsad3"));
+        assertEquals(null, StringTasks.simpleValueOf("1-easdsad3"));
+        assertEquals(null, StringTasks.simpleValueOf("1e--asdsad3"));
+        assertEquals(null, StringTasks.simpleValueOf("--1easdsad3"));
+        assertEquals(null, StringTasks.simpleValueOf("-1e-asdsad3-"));
+        assertEquals(null, StringTasks.simpleValueOf("1easdsad3-"));
         assertEquals(1.2e-3, StringTasks.simpleValueOf("ccz1asd.zc2deg-h3j"));
         assertEquals(null, StringTasks.simpleValueOf("ccz1asd.zc2de.g-h3j"));
         assertEquals(1.3, StringTasks.simpleValueOf("fff1fdf.asdsad3"));
