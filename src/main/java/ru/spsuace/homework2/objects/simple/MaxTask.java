79645cc7c26aefa7bf79ae49fc0665a866d128ba
Aleksandr Galkin <alexander.galkin@corp.mail.ru>
2020-03-16 13:00:53 +0300

    fix tests

--- src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
+++ src/main/java/ru/spsuace/homework2/objects/simple/MaxTask.java
@@ -1,5 +1,7 @@
 package ru.spsuace.homework2.objects.simple;
 
+import java.util.Arrays;
+
 public class MaxTask {
 
     /**
@@ -8,9 +10,23 @@ public class MaxTask {
      * Если длина массива меньше count, то вернуть null
      * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
      * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
+     * Можно пользоваться Arrays.sort(arr), эта функция сортирует входящий массив
      *
      */
-    public static int[] getMaxArray(int[] array, int count) {
+    public static int[] getMaxArraySimple(int[] array, int count) {
+        return null;
+    }
+
+    /**
+     * Вам дан массив и количество элементов в возвращаемом массиве
+     * Вернуть нужно массив из count максимальных элементов array, упорядоченный по убыванию.
+     * Если длина массива меньше count, то вернуть null
+     * Например ({1, 3, 10, 11, 22, 0}, 2) -> {22, 11}
+     * ({1, 3, 22, 11, 22, 0}, 3) -> {22, 22, 11}
+     * Нельзя пользоваться Arrays.sort
+     *
+     */
+    public static int[] getMaxArrayHard(int[] array, int count) {
         return null;
     }
 
--- src/test/java/ru/spsuace/homework2/simple/MaxTaskTest.java
+++ src/test/java/ru/spsuace/homework2/simple/MaxTaskTest.java
@@ -9,13 +9,24 @@ import static org.junit.Assert.*;
 public class MaxTaskTest {
 
     @Test
-    public void getMaxArray() {
-        assertArrayEquals(new int[] {}, MaxTask.getMaxArray(new int[] {}, 0));
-        assertArrayEquals(new int[] {}, MaxTask.getMaxArray(new int[] {1, 2, 3}, 0));
-        assertArrayEquals(null, MaxTask.getMaxArray(new int[] {1, 2, 3}, 4));
-        assertArrayEquals(new int[] {22, 11}, MaxTask.getMaxArray(new int[] {1, 3, 10, 11, 22, 0}, 2));
-        assertArrayEquals(new int[] {22, 22, 11}, MaxTask.getMaxArray(new int[] {1, 3, 22, 11, 22, 0}, 3));
-        assertArrayEquals(new int[] {3, 2, 1}, MaxTask.getMaxArray(new int[] {1, 2, 3}, 3));
-        assertArrayEquals(new int[] {10, 9, 8, 8}, MaxTask.getMaxArray(new int[] {5, 7, 1, 8, 9, 1, 4, 5, 10, 2, 8, 3, 1}, 4));
+    public void getMaxArraySimple() {
+        assertArrayEquals(new int[] {}, MaxTask.getMaxArraySimple(new int[] {}, 0));
+        assertArrayEquals(new int[] {}, MaxTask.getMaxArraySimple(new int[] {1, 2, 3}, 0));
+        assertArrayEquals(null, MaxTask.getMaxArraySimple(new int[] {1, 2, 3}, 4));
+        assertArrayEquals(new int[] {22, 11}, MaxTask.getMaxArraySimple(new int[] {1, 3, 10, 11, 22, 0}, 2));
+        assertArrayEquals(new int[] {22, 22, 11}, MaxTask.getMaxArraySimple(new int[] {1, 3, 22, 11, 22, 0}, 3));
+        assertArrayEquals(new int[] {3, 2, 1}, MaxTask.getMaxArraySimple(new int[] {1, 2, 3}, 3));
+        assertArrayEquals(new int[] {10, 9, 8, 8}, MaxTask.getMaxArraySimple(new int[] {5, 7, 1, 8, 9, 1, 4, 5, 10, 2, 8, 3, 1}, 4));
+    }
+
+    @Test
+    public void getMaxArrayHard() {
+        assertArrayEquals(new int[] {}, MaxTask.getMaxArrayHard(new int[] {}, 0));
+        assertArrayEquals(new int[] {}, MaxTask.getMaxArrayHard(new int[] {1, 2, 3}, 0));
+        assertArrayEquals(null, MaxTask.getMaxArrayHard(new int[] {1, 2, 3}, 4));
+        assertArrayEquals(new int[] {22, 11}, MaxTask.getMaxArrayHard(new int[] {1, 3, 10, 11, 22, 0}, 2));
+        assertArrayEquals(new int[] {22, 22, 11}, MaxTask.getMaxArrayHard(new int[] {1, 3, 22, 11, 22, 0}, 3));
+        assertArrayEquals(new int[] {3, 2, 1}, MaxTask.getMaxArrayHard(new int[] {1, 2, 3}, 3));
+        assertArrayEquals(new int[] {10, 9, 8, 8}, MaxTask.getMaxArrayHard(new int[] {5, 7, 1, 8, 9, 1, 4, 5, 10, 2, 8, 3, 1}, 4));
     }
 }
